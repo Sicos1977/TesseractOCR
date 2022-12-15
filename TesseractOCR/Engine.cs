bec532edc6f49d688f9fab2a9b731f6116937868
Kees van Spelde <keesvanspelde@hotmail.com>
2022-12-15 20:24:13 +0100

    Update package info

--- TesseractOCR/Engine.cs
+++ TesseractOCR/Engine.cs
@@ -115,7 +115,7 @@ namespace TesseractOCR
         /// <param name="logger">When set then logging is written to this <see cref="ILogger"/> interface</param>
         public Engine(string dataPath, Language language, EngineMode engineMode = EngineMode.Default, IEnumerable<string> configFiles = null, IDictionary<string, object> initialOptions = null, bool setOnlyNonDebugVariables = false, ILogger logger = null)
         {
-            Engine(dataPath, new List<Language> {language}, engineMode, configFiles, initialOptions, setOnlyNonDebugVariables, logger);
+            Initialize(dataPath, new List<Language> {language}, engineMode, configFiles, initialOptions, setOnlyNonDebugVariables, logger);
         }
 
         /// <summary>
